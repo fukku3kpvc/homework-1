d0abdf88ffe6203590a8c05f17cce48f85e2b476
fukk.u.3kpvc <ykwnd@yahoo.com>
2018-12-13 14:37:07 +0500

    Bug fixed

--- first.rb
+++ first.rb
@@ -1,12 +1,12 @@
-def password_randomizer(len)
+def password_randomizer(len = 6)
   symbols = ['a'..'z', 'A'..'Z', 0..9].map { |range| range.to_a }.flatten
-  puts((0..len).map { symbols.sample }.shuffle.join)
+  puts((0..len - 1).map { symbols.sample }.shuffle.join)
 end
 
-print('Write password length or skip with Enter or 0 to default: ')
+print('Write password length or skip with Enter to default: ')
 len = gets.to_i
-if len == '' or len == 0
-  password_randomizer(5)
+if len.zero?
+  password_randomizer
 else
   password_randomizer(len)
 end
